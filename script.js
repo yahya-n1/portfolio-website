1d7354318f99e2885577f8d0edf7917353d897d9
yahya-n1 <naway@farmingdale.edu>
2026-03-09 10:02:25 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("contactForm");
+
+if(form){
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    document.getElementById("formMessage").innerText =
+      "Thank you! Your message has been received.";
+  });
+}
